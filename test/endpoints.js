2e31bb07eb3f3ab10944b9207ae2196bcdd7d751
Ahmed Kamal <en.ahmed.mk@gmail.com>
2021-03-18 20:52:07 +0200

    feat: Add post a target route

--- test/endpoints.js
+++ test/endpoints.js
@@ -15,3 +15,28 @@ test.serial.cb('healthcheck', function (t) {
     t.end()
   })
 })
+
+test.serial.cb('should post a target', function (t) {
+  var url = '/api/targets'
+  var opts = { method: 'POST', encoding: 'json' }
+
+  servertest(server(), url, opts, onResponse)
+    .end(JSON.stringify(testClient))
+
+  function onResponse (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.like(res.body, testClient, 'body is ok')
+    t.end()
+  }
+})
+
+var testClient = {
+  url: 'http://example.com',
+  value: '0.50',
+  maxAcceptsPerDay: '10',
+  accept: {
+    geoState: { $in: ['ca', 'ny'] },
+    hour: { $in: ['13', '14', '15'] }
+  }
+}
